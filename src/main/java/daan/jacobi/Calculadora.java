a09d39059c6bae170e0006a10e21362dfcb3fffc
Sanja <Sanja@DAAN>
2022-11-27 16:51:58 -0600

    jacobi initial

---
+++ src/main/java/daan/jacobi/Calculadora.java
@@ -0,0 +1,64 @@
+
+package daan.jacobi;
+
+public class Calculadora {
+    double[][] matriz;
+    double[] definido, temporal, valor;
+    
+    public Calculadora(){
+    }
+    
+    public Calculadora(double[][] matriz, double[] definido){
+        setMatriz(matriz);
+        setDefinido(definido);
+        setValor();
+        setTemp();
+    }
+    
+    void cal(){
+        for(int i = 0; i < matriz.length;i++){
+            for(int j = 0; j < matriz[0].length; j++){
+                if(i != j){
+                    if(j != matriz[0].length - 1){
+                        temporal[i] += (matriz[i][j] * definido[j]) / valor[i];
+                    }else{
+                        temporal[i] =+ matriz[i][j] / valor[i];
+                        System.out.println("obtenido");
+                    }     
+                }
+            }
+        }
+    }
+    
+    public void setMatriz(double[][] matriz){
+        this.matriz = matriz;
+    }
+    public void setDefinido(double[] definido){
+        this.definido = definido;
+    }
+    
+    public void setValor(){
+        this.valor = new double[matriz.length];
+        
+        for(int i = 0; i < matriz.length; i++){
+            this.valor[i] = this.matriz[i][i];
+        }
+    }
+    
+    public void setTemp(){
+        this.temporal = new double [matriz.length];
+        cal();
+    }
+    
+    double[][] getMatriz(){
+        return matriz;
+    }
+    
+    double[] getDefinido(){
+        return definido;
+    }
+    
+    double[] getTemp(){
+        return temporal;
+    }
+}
